d8a99e346db166d8c776774a10566e71b6354df5
neo.chen <neo.chen@derbysoft.com>
2016-11-18 09:11:13 +0000

    add maven config to run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-docker run --rm -it -v `pwd`/../:/usr/src/fileserver -v maven:/root/.m2 -w /usr/src/fileserver maven:3-jdk-8 mvn package && docker-compose up
+# 1. if there are already running or stopped container services, we should stop it
+echo " removing the old containers ..."
+docker-compose down > /dev/null 2>&1 
+
+# 2. we inspect the system's maven local repository, and create an container which map the host's repository to the container's volume for later use
+docker create -v `mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]'`:/root/.m2 --name=maven busybox:1.25 /bin/true > /dev/null 2>&1
+
+# 3. build the src files and start the docker-compose 
+echo " going to compile all the sources, and start up the containers ..."
+docker run --rm -it --net=bridge --volumes-from maven -v `pwd`/../:/usr/src/fileserver -w /usr/src/fileserver maven:3-jdk-8 mvn package && docker-compose up
